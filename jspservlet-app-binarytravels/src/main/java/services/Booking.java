9f6762af0fd6b7f075cbe6ba5ac009abeece102a
Iamneo <techadmin@iamneo.ai>
2023-09-05 09:05:49 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/Booking.java
+++ jspservlet-app-binarytravels/src/main/java/services/Booking.java
@@ -2,10 +2,5 @@ package services;
 
 public class Booking{
 //	Enter code here...
-    public double booking(int noOfPersons, int rates){
-        double totalRate;
-        totalRate = noOfPersons * rates;
-        return totalRate;
-    }
 }
 
--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -8,42 +8,5 @@ import model.Hotel;
 import model.Train;
 
 public class FareCalculator extends Booking{
-
-    public double book(Flight flight) {
-
-        double totalFare = booking(flight.getNoOfPersons(),flight.getRates());
-
-        if(flight.getTriptype().equalsIgnoreCase("one-way")){
-            return totalFare;
-        }else{
-            int comp = (flight.getFrom()).compareTo(flight.getTo());
-            if(comp < 0)
-                return 2 * totalFare;
-            else
-                return 0;
-        }
-    }
-
-    public double book(Bus bus) {
-        double totalFare = booking(bus.getNoOfPersons(),bus.getRates());
-        return totalFare;
-    }
-
-    public double book(Hotel hotel) {
-        double totalFare = 0;
-        if(hotel.getFromdate().isBefore(hotel.getTodate())){
-            totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
-        }
-        double fare = hotel.getRates();
-        if(fare == 20000.0)
-            return 90000.0;
-
-        return 7350.0;
-    }
-
-    public double book(Train train) {
-        double totalFare = booking(train.getNoOfPersons(),train.getRates());
-        return totalFare;
-    }
-
+//Enter code here...
 }
